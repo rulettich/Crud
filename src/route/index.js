6bf3ea95f4c3d55bb33762344551b37aa14e818b
rulettich <rulettich@gmail.com>
2024-03-26 19:20:04 +0200

    crud: product

--- src/route/index.js
+++ src/route/index.js
@@ -4,6 +4,64 @@ const express = require('express')
 const router = express.Router()
 
 // ================================================================
+class Product {
+  static #list = []
+
+  constructor(name, price, description) {
+    this.id = Math.floor(Math.random() * 90000) + 10000
+    this.createDate = new Date().toISOString()
+    this.name = name
+    this.price = price
+    this.description = description
+  }
+
+  static add = (product) => {
+    this.#list.push(product)
+  }
+
+  static getList = () => this.#list
+
+  static getById = (id) =>
+    this.#list.find((product) => product.id === id)
+
+  static deleteById = (id) => {
+    const index = this.#list.findIndex(
+      (product) => product.id === id,
+    )
+    if (index !== -1) {
+      this.#list.splice(index, 1)
+      return true
+    } else {
+      return false
+    }
+  }
+
+  static updateById = (
+    id,
+    { price, name, description },
+  ) => {
+    const product = this.getById(id)
+
+    if (product) {
+      if (price) {
+        product.price = price
+      }
+
+      if (name) {
+        product.name = name
+      }
+
+      if (description) {
+        product.description = description
+      }
+
+      return true
+    } else {
+      return false
+    }
+  }
+}
+//=================================================================
 
 // router.get Створює нам один ентпоїнт
 
@@ -19,7 +77,106 @@ router.get('/', function (req, res) {
   // ↑↑ сюди вводимо JSON дані
 })
 
+//=================================================================
+
+// ↙️ тут вводимо шлях (PATH) до сторінки
+router.get('/product-edit', function (req, res) {
+  const { id } = req.query
+
+  console.log(id)
+
+  res.render('product-edit', {
+    style: 'product-edit',
+  })
+})
+
 // ================================================================
+
+router.post('/product-edit', function (req, res) {
+  const { name, price, description, id } = req.body
+
+  console.log(name, price, description, id)
+
+  const result = Product.updateById(Number(id), {
+    name,
+    price,
+    description,
+  })
+
+  res.render('alertEdit', {
+    style: 'alertEdit',
+    info: result,
+  })
+})
+
+//=================================================================
+
+router.get('/product-list', function (req, res) {
+  //   // res.render генерує нам HTML сторінку
+
+  const list = Product.getList()
+
+  //   // ↙️ cюди вводимо назву файлу з сontainer
+  res.render('product-list', {
+    //     // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+    style: 'product-list',
+
+    data: {
+      products: {
+        list,
+        isEmpty: list.length === 0,
+      },
+    },
+  })
+  //   // ↑↑ сюди вводимо JSON дані
+})
+
+//=================================================================
+
+router.post('/product-create', function (req, res) {
+  const { name, price, description } = req.body
+
+  const product = new Product(name, price, description)
+
+  Product.add(product)
+
+  console.log(Product.getList())
+
+  res.render('alert', {
+    style: 'alert',
+  })
+})
+
+//=================================================================
+
+router.get('/product-create', function (req, res) {
+  const list = Product.getList()
+
+  res.render('alert', {
+    style: 'alert',
+
+    data: {
+      products: {
+        list,
+        isEmpty: list.length === 0,
+      },
+    },
+  })
+})
+
+//=================================================================
+
+router.get('/product-delete', function (req, res) {
+  const { id } = req.query
+
+  Product.deleteById(Number(id))
+
+  res.render('product-delete', {
+    style: 'product-delete',
+  })
+})
+
+//=================================================================
 
 // Підключаємо роутер до бек-енду
 module.exports = router
